a417ca79740370d30b3cd2bffc9c4c2672b60022
Jolly-cole <vrwt22@gmail.com>
2021-07-08 18:21:39 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("Hello world")
+});
+
+app.listen(5000,()=>{
+    console.log("Server started on port 5000")
+})
+
+
+
