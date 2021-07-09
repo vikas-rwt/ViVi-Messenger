c395a52637e6e2d4b831d193c52577e17e8212f5
Wicky-boy <vigneshrgm962950@gmial.com>
2021-07-09 10:35:55 +0530

    Added user login_system

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const { urlencoded } = require("express")
+const db = require("./database.js")
+const  PORT = 5000 || process.env.PORT
+
 
 const app = express()
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get("/",(req,res)=>{
     res.send("Hello world")
 });
 
-app.listen(5000,()=>{
-    console.log("Server started on port 5000")
+app.post("/register",(req,res)=>{
+    db.register(req.body.name,req.body.email,req.body.password,req.body.age,function(error,call_back){
+        if(error){
+            console.log(error)
+        }else{
+            console.log(`CB : ${call_back}`)
+        }
+    })
+    res.send("Registered")
+});
+
+
+app.post("/login",(req,res)=>{
+    db.login(req.body.email,req.body.password,function(error,authe){
+        if(error){
+            console.log(`Error : ${error}`)
+        }else{
+            console.log(`Login : ${authe}`)
+        }
+    })
+    res.send("done")
+});
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server started on port ${PORT}`)
 })
---
+++ database.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+
+mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true ,useUnifiedTopology:true})
+mongoose.connection.on("connected",()=>{
+    console.log("Mongodb connected to atlas cloud successfully...")
+})
+
+userSchema = {
+    "fullName":String,
+    "email":String,
+    "password":String,
+    "age":Number
+}
+userDetails = mongoose.model("user_details",userSchema)
+
+exports.login = (user_email,user_password,cb)=>{
+    console.log(user_email,user_password)
+    userDetails.find({email:user_email},(error,foundItems)=>{
+        if(error){
+            console.log(err)
+            return status=501
+        }else{
+            if(foundItems.length === 0){
+                cb(false)
+                return 
+            }else{
+                bcrypt.compare(user_password, foundItems[0].password, function(err, result) {
+                    if(err){
+                        console.log(err)
+                        return status=501
+                    }else{
+                        console.log(`Result : ${result}`)
+                        cb(null,result)
+                    }
+            });
+            }
+
+        }
+    })
+
+}
+
+exports.register = (user_name,user_email,user_password,user_age,cb)=>{
+    bcrypt.hash(user_password, 10, function(err, hash) {
+        userDetails.insertMany({fullName:user_name,email:user_email,password:hash,age:user_age},(err)=>{
+            if(err){
+                console.log(err)
+                cb(err)
+            }else{
+                console.log("Successfully inserted into the Database")
+                cb(null,"Registered")
+            }
+        })
+    });
+}
